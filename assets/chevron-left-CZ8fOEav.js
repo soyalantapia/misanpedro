923ddcba896a78249e8502690b9d69f1826903e4
soyalantapia <alannaimtapia@gmail.com>
2026-06-05 19:32:27 -0300

    Deploy web: 2026-06-05-1932

---
+++ assets/chevron-left-CZ8fOEav.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-Dg-iW3F8.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=e("chevron-left",o);export{t as C};
---
+++ assets/mockData-HeC4tpKx.js
@@ -0,0 +1,1 @@
+import{aJ as t}from"./index-Dg-iW3F8.js";const o=t;function e(n){return t.find(r=>r.id===n)}export{o as M,e as g};
